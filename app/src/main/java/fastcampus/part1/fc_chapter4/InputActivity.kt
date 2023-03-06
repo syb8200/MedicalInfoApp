19ab33abf43dcadcaccd6f958d76b370b880bd4e
syb8200 <syb8200@naver.com>
2023-03-06 18:30:08 +0900

    SharedPreferences 사용하여 데이터 저장하기

--- app/src/main/java/fastcampus/part1/fc_chapter4/InputActivity.kt
+++ app/src/main/java/fastcampus/part1/fc_chapter4/InputActivity.kt
@@ -2,9 +2,11 @@ package fastcampus.part1.fc_chapter4
 
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import fastcampus.part1.fc_chapter4.databinding.ActivityInputBinding
@@ -46,5 +48,39 @@ class InputActivity : AppCompatActivity() {
             binding.warningEditText.isVisible = isChecked
         }
         binding.warningEditText.isVisible = binding.warningCheckBox.isChecked
+
+        // 저장 버튼
+        binding.saveButton.setOnClickListener {
+            saveData()
+            finish() // Activity 종료
+        }
+
+    }
+
+    private fun saveData() {
+        // Context.MODE_PRIVATE : 이 파일을 생성한 앱에서만 접근 가능
+        // commit()과 apply()로 데이터를 저장할 수 있지만 -> apply() 권장!
+        // commit() : 메인 스레드에서 작업을 하다보니 사용자가 데이터를 저장하는 동안 다른 작업을 진행할 수 없음
+        // apply() : commit()과 달리 다른 스레드에서 작업이 이루어짐 (비동기적)
+        with(getSharedPreferences("userInformation", Context.MODE_PRIVATE).edit()) {
+            putString("name", binding.nameEditText.text.toString())
+            putString("bloodType", getBloodType())
+            putString("emergencyContact", binding.emergencyContactEditText.text.toString())
+            putString("birthDate", binding.birthDateValueTextView.text.toString())
+            putString("warning", getWarning())
+            apply()
+        }
+
+        Toast.makeText(this, "저장을 완료했습니다!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getBloodType(): String {
+        val bloodAlphabet = binding.bloodTypeSpinner.selectedItem.toString()
+        val bloodSign = if (binding.bloodTypePlus.isChecked) "+" else "-"
+        return "$bloodSign $bloodAlphabet"
+    }
+
+    private fun getWarning(): String {
+        return if (binding.warningCheckBox.isChecked) binding.warningEditText.text.toString() else ""
     }
 }
