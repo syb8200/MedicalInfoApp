e786f1cbb9402b72af4c82e63b58e190cd806c35
syb8200 <syb8200@naver.com>
2023-03-06 18:45:20 +0900

    기존 string → Const로 변경

---
+++ app/src/main/java/fastcampus/part1/fc_chapter4/Const.kt
@@ -0,0 +1,8 @@
+package fastcampus.part1.fc_chapter4
+
+const val USER_INFORMATION = "userInformation"
+const val NAME = "name"
+const val BIRTHDATE = "birthDate"
+const val BLOOD_TYPE = "bloodType"
+const val EMERGENCY_CONTACT = "emergencyContact"
+const val WARNING = "warning"
