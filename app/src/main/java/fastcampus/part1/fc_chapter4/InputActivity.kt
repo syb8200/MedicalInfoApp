78aff9d65bcf9a704390b9aed47fb90ddc1abe3f
syb8200 <syb8200@naver.com>
2023-03-06 16:17:44 +0900

    InputActivity viewBinding 및 Spinner 구현

--- app/src/main/java/fastcampus/part1/fc_chapter4/InputActivity.kt
+++ app/src/main/java/fastcampus/part1/fc_chapter4/InputActivity.kt
@@ -2,15 +2,28 @@ package fastcampus.part1.fc_chapter4
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import fastcampus.part1.fc_chapter4.databinding.ActivityInputBinding
 
 class InputActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityInputBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input)
+
+        binding = ActivityInputBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         // intent로 데이터 넘겨받기
         val message = intent.getStringExtra("intentMessage") ?: "없음"
         Log.d("intentMessage", message)
+
+        // 혈액형 스피너 구현
+        binding.bloodTypeSpinner.adapter = ArrayAdapter.createFromResource(
+            this, R.array.blood_types, android.R.layout.simple_list_item_1
+        )
     }
 }
